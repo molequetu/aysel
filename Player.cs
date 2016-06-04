5e214a33b56c06e5079f2b8d51506277c35fe66f
ibraimsap <ibraimsap@gmail.com>
2016-06-04 16:33:37 +0300

    added player props, and ovveride GetCurrentTilePos of Character

--- Player.cs
+++ Player.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml;
+using System.Drawing;
 
 namespace Aysel
 {
-    class Player
+    class Player : Character
     {
+
+        private Game p_game;
+        private int p_gold;
+
+        public Player(ref Game game) : base(ref game)
+        {
+            p_game = game;
+            p_gold = 0;
+        }
+
+        public int Gold
+        {
+            get { return p_gold; }
+            set { p_gold = value; }
+        }
+
+        //overrides Character.GetCurrentTilePos due to ScrollPos
+        public override Point GetCurrentTilePos()
+        {
+            PointF feet = p_game.Hero.FootPos;
+            int tilex = (int)(p_game.World.ScrollPos.X + feet.X) / 32;
+            int tiley = (int)(p_game.World.ScrollPos.Y + feet.Y) / 32;
+            return new Point(tilex, tiley);
+        }
+
+
+        public override string ToString()
+        {
+            return base.Name;
+        }
+
+        public void LoadGame(string filename)
+        {
+        }
+
+        public void SaveGame(string filename)
+        {
+        }
     }
 }
