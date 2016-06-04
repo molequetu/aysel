5f3e27ed1600f99195caa0092e90b04e9e8e5e20
ibraimsap <ibraimsap@gmail.com>
2016-06-04 19:18:54 +0300

    minnor fixes

--- Form1.cs
+++ Form1.cs
@@ -46,10 +46,12 @@ namespace Aysel
             createGame();
             // create the map
             createMap();
-
-           // create hero and player Aysel
+            
+            // create player Aysel
             createAysel();
-
+            // spawn Aysel on entry
+            spawnAyselOnTileEntry();
+           
             // load ilhan, the quest manager
             createIlhan();
 
@@ -146,32 +148,32 @@ namespace Aysel
     
        private void updateHeroDir()
        {
-           if (keyState.up && keyState.right)
-               aysel.Direction = 1;
-           else if (keyState.right && keyState.down)
-               aysel.Direction = 3;
-           else if (keyState.down && keyState.left)
-               aysel.Direction = 5;
-           else if (keyState.left && keyState.up)
-               aysel.Direction = 7;
-           else if (keyState.up) aysel.Direction = 0;
-           else if (keyState.right) aysel.Direction = 2;
-           else if (keyState.down) aysel.Direction = 4;
-           else if (keyState.left) aysel.Direction = 6;
-           else aysel.Direction = -1;
+           if (game.keyState.up && game.keyState.right)
+                game.aysel.Direction = 1;
+           else if (game.keyState.right && game.keyState.down)
+               game.aysel.Direction = 3;
+           else if (game.keyState.down && game.keyState.left)
+               game.aysel.Direction = 5;
+           else if (game.keyState.left && game.keyState.up)
+                game.aysel.Direction = 7;
+           else if (game.keyState.up) game.aysel.Direction = 0;
+           else if (game.keyState.right) game.aysel.Direction = 2;
+           else if (game.keyState.down) game.aysel.Direction = 4;
+           else if (game.keyState.left) game.aysel.Direction = 6;
+           else game.aysel.Direction = -1;
 
        }
         private void doAysel()
         {
             //limit player sprite to the screen boundary
-            if (aysel.X < -32) aysel.X = -32;
-            else if (aysel.X > 800 - 65) aysel.X = 800 - 65;
-            if (aysel.Y < -48) aysel.Y = -48;
-            else if (aysel.Y > 600 - 81) aysel.Y = 600 - 81;
+            if (game.aysel.X < -32) game.aysel.X = -32;
+            else if (game.aysel.X > 800 - 65) game.aysel.X = 800 - 65;
+            if (game.aysel.Y < -48) game.aysel.Y = -48;
+            else if (game.aysel.Y > 600 - 81) game.aysel.Y = 600 - 81;
 
             // orient aysel to the right direction  and draw
             updateHeroDir();
-            aysel.Draw();
+            game.aysel.Draw();
 
         }
         /*
@@ -185,18 +187,18 @@ namespace Aysel
             Pen color;
 
             //draw the vendor sprite
-            if (ilhan.X > map.ScrollPos.X &&
-                ilhan.X < map.ScrollPos.X + 23 * 32 &&
-                ilhan.Y > map.ScrollPos.Y &&
-                ilhan.Y < map.ScrollPos.Y + 17 * 32)
+            if (ilhan.X > game.map.ScrollPos.X &&
+                ilhan.X < game.map.ScrollPos.X + 23 * 32 &&
+                ilhan.Y > game.map.ScrollPos.Y &&
+                ilhan.Y < game.map.ScrollPos.Y + 17 * 32)
             {
-                relativeX = Math.Abs(map.ScrollPos.X - ilhan.X);
-                relativeY = Math.Abs(map.ScrollPos.Y - ilhan.Y);
+                relativeX = Math.Abs(game.map.ScrollPos.X - ilhan.X);
+                relativeY = Math.Abs(game.map.ScrollPos.Y - ilhan.Y);
                 ilhan.GetSprite.Draw((int)relativeX, (int)relativeY);
             }
 
             //get center of hero sprite
-            PointF heroCenter = aysel.FootPos;
+            PointF heroCenter = game.aysel.FootPos;
             heroCenter.X += 16;
             heroCenter.Y += 16;
             game.Device.DrawRectangle(Pens.Red, heroCenter.X - 2,
@@ -257,14 +259,14 @@ namespace Aysel
             ilhanDialogue.setButtonText(1, "Thank you, Ilhan");
           
             //reposition dialogue window 
-            if (aysel.CenterPos.X < 400)
+            if (game.aysel.CenterPos.X < 400)
             {
-                if (aysel.CenterPos.Y < 300)
+                if (game.aysel.CenterPos.Y < 300)
                     ilhanDialogue.setCorner(Dialogue.Positions.LowerRight);
                 else
                     ilhanDialogue.setCorner(Dialogue.Positions.UpperRight);
             } else {
-                if (aysel.CenterPos.Y < 300)
+                if (game.aysel.CenterPos.Y < 300)
                     ilhanDialogue.setCorner(Dialogue.Positions.LowerLeft);
                 else
                     ilhanDialogue.setCorner(Dialogue.Positions.UpperLeft);
@@ -289,57 +291,57 @@ namespace Aysel
             int steps = 4;
 
             // take maps scroll position
-            PointF pos =  map.ScrollPos;
+            PointF pos = game.map.ScrollPos;
 
             //up key movement
-            if (keyState.up)
+            if (game.keyState.up)
             {
-                if (aysel.Y > 300 - 48) aysel.Y -= steps;
+                if (game.aysel.Y > 300 - 48) game.aysel.Y -= steps;
                 else
                 {
                     pos.Y -= steps;
-                    if (pos.Y <= 0) aysel.Y -= steps;
+                    if (pos.Y <= 0) game.aysel.Y -= steps;
                 }
 
             }
             //down key movement
-            else if (keyState.down)
+            else if (game.keyState.down)
             {
-                if (aysel.Y < 300 - 48)
-                    aysel.Y += steps;
+                if (game.aysel.Y < 300 - 48)
+                    game.aysel.Y += steps;
                 else
                 {
                     pos.Y += steps;
-                    if (pos.Y >= (127 - 19) * 32) aysel.Y += steps;
+                    if (pos.Y >= (127 - 19) * 32) game.aysel.Y += steps;
                 }
             }
 
             //left key movement
-            if (keyState.left)
+            if (game.keyState.left)
             {
-                if (aysel.X > 400 - 48) aysel.X -= steps;
+                if (game.aysel.X > 400 - 48) game.aysel.X -= steps;
                 else
                 {
                     pos.X -= steps;
-                    if (pos.X <= 0) aysel.X -= steps;
+                    if (pos.X <= 0) game.aysel.X -= steps;
                 }
             }
             //right key movement
-            else if (keyState.right)
+            else if (game.keyState.right)
             {
-                if (aysel.X < 400 - 48) aysel.X += steps;
+                if (game.aysel.X < 400 - 48) game.aysel.X += steps;
                 else
                 {
                     pos.X += steps;
-                    if (pos.X >= (127 - 25) * 32) aysel.X += steps;
+                    if (pos.X >= (127 - 25) * 32) game.aysel.X += steps;
                 }
             }
 
             // set posisition to level scroll position and then update level
            // if (pos.X < 0) pos.X = 0;
             //if (pos.Y < 0) pos.Y = 0;
-           // map.ScrollPos = pos;
-           // map.Update();
+            game.map.ScrollPos = pos;
+            game.map.Update();
 
            
 
@@ -347,12 +349,12 @@ namespace Aysel
             int frameRate = game.FrameRate();
 
             //drawing code should be limited to 60 fps
-            ticks = Environment.TickCount;
+            int ticks = Environment.TickCount;
             if (ticks > drawLast + 16)
             {
                 drawLast = ticks;
                 // draw the map
-                map.Draw(0, 0, 800, 600);
+                game.map.Draw(0, 0, 800, 600);
                 
                 // draw and animate aysel
                 doAysel();
@@ -365,17 +367,17 @@ namespace Aysel
                 //print stats
                 game.Print(700, 0, frameRate.ToString());
                 int y = 0;
-                game.Print(0, y, "Scroll " + map.ScrollPos.ToString());
+                game.Print(0, y, "Scroll " + game.map.ScrollPos.ToString());
                 y += 20;
-                game.Print(0, y, "Player " + aysel.Position.ToString());
+                game.Print(0, y, "Player " + game.aysel.Position.ToString());
                 y += 20;
 
 
                 Point feet = HeroFeet();
 
-                int tilex = (int)(map.ScrollPos.X + feet.X) / 32;
-                int tiley = (int)(map.ScrollPos.Y + feet.Y) / 32;
-                Level.tilemapStruct ts = map.getTile(tilex, tiley);
+                int tilex = (int)(game.map.ScrollPos.X + feet.X) / 32;
+                int tiley = (int)(game.map.ScrollPos.Y + feet.Y) / 32;
+                Level.tilemapStruct ts = game.map.getTile(tilex, tiley);
                 game.Print(0, y, "Tile " + tilex.ToString() + "," +
                     tiley.ToString() + " = " + ts.tilenum.ToString());
                 y += 20;
@@ -387,7 +389,7 @@ namespace Aysel
             
 
                 //highlight collision areas around player
-                game.Device.DrawRectangle(Pens.Blue, aysel.GetSprite.Bounds);
+                game.Device.DrawRectangle(Pens.Blue, game.aysel.GetSprite.Bounds);
                 game.Device.DrawRectangle(Pens.Red, feet.X + 16 - 1,
                     feet.Y + 16 - 1, 2, 2);
                 game.Device.DrawRectangle(Pens.Red, feet.X, feet.Y, 32, 32);
@@ -404,7 +406,7 @@ namespace Aysel
         }
         private Point HeroFeet()
         {
-            return new Point((int)(aysel.X + 32), (int)(aysel.Y + 32 + 16));
+            return new Point((int)(game.aysel.X + 32), (int)(game.aysel.Y + 32 + 16));
         }
 
 
@@ -418,13 +420,13 @@ namespace Aysel
             switch (e.KeyCode)
             {
                 case Keys.Up:
-                case Keys.W: keyState.up = true; break;
+                case Keys.W: game.keyState.up = true; break;
                 case Keys.Down:
-                case Keys.S: keyState.down = true; break;
+                case Keys.S: game.keyState.down = true; break;
                 case Keys.Left:
-                case Keys.A: keyState.left = true; break;
+                case Keys.A: game.keyState.left = true; break;
                 case Keys.Right:
-                case Keys.D: keyState.right = true; break;
+                case Keys.D: game.keyState.right = true; break;
                 case Keys.Space: talkFlag = true; break;
 
             }
@@ -436,22 +438,22 @@ namespace Aysel
             {
                 case Keys.Escape: game.GameOver = true; break;
                 case Keys.Up:
-                case Keys.W: keyState.up = false; break;
+                case Keys.W: game.keyState.up = false; break;
                 case Keys.Down:
-                case Keys.S: keyState.down = false; break;
+                case Keys.S: game.keyState.down = false; break;
                 case Keys.Left:
-                case Keys.A: keyState.left = false; break;
+                case Keys.A: game.keyState.left = false; break;
                 case Keys.Right:
-                case Keys.D: keyState.right = false; break;
+                case Keys.D: game.keyState.right = false; break;
                 case Keys.Space: talkFlag = false; break;
                 case Keys.D1:
-                    aysel.AnimationState = Character.AnimationStates.Walking;
+                    game.aysel.AnimationState = Character.AnimationStates.Walking;
                     break;
                 case Keys.D2:
-                    aysel.AnimationState = Character.AnimationStates.Attacking;
+                    game.aysel.AnimationState = Character.AnimationStates.Attacking;
                     break;
                 case Keys.D3:
-                    aysel.AnimationState = Character.AnimationStates.Dying;
+                    game.aysel.AnimationState = Character.AnimationStates.Dying;
                     break;
                 
             }
